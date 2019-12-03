1044537db312aae09099ffb1241094cb9318760d
Håvard Anda Estensen <haavard.ae@gmail.com>
2019-12-03 12:58:23 +0100

    Leftpad user query

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # tmp-go-meetup
+
+## Example
+```
+$ curl -X GET "localhost:8080/leftpad?str=hello&padding=10"
+"          hello"
+```
--- main.go
+++ main.go
@@ -2,37 +2,60 @@ package main
 
 import (
 	"encoding/json"
-	"io/ioutil"
 	"log"
 	"net/http"
+	"strconv"
 )
 
 func main() {
+	leftpadHandler()
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func leftpadHandler() {
 	http.HandleFunc("/leftpad", func(w http.ResponseWriter, r *http.Request) {
-		// TODO: Is GET correct here?
+		log.Print("Incoming request: ", r.RequestURI)
+
 		if r.Method != "GET" {
 			log.Printf("%s on %s is not allowed\n", r.Method, "/leftpad")
 			http.Error(w, http.StatusText(405), 405)
 			return
 		}
 
-		// Read without buffer
-		body, err := ioutil.ReadAll(r.Body)
+		str := r.FormValue("str")
+		if str == "" {
+			log.Println("no string")
+			http.Error(w, http.StatusText(400), 400)
+			return
+		}
+
+		padding := r.FormValue("padding")
+		if padding == "" {
+			log.Println("no padding")
+			http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+			return
+		}
+
+		str, err := leftpad(str, padding)
 		if err != nil {
-			log.Fatalln(err)
+			log.Println("padding NaN")
+			http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+			return
 		}
 
-		var dat map[string]interface{}
-		if err := json.Unmarshal(body, &dat); err != nil {
-			respondWithJSON(w, http.StatusInternalServerError, "")
-		}
-		log.Println(dat)
-		
-		// respond
-		respondWithJSON(w, http.StatusOK, dat["txt"])
+		respondWithJSON(w, http.StatusOK, str)
 	})
+}
 
-	log.Fatal(http.ListenAndServe(":8080", nil))
+func leftpad(str string,padding string) (string, error) {
+	numPadding, err := strconv.Atoi(padding)
+	if err != nil {
+		return "", err
+	}
+	for i := 0; i < numPadding; i++ {
+		str = " " + str
+	}
+	return str, nil
 }
 
 func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
